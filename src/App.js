43d305a3b3467b24c7e3d06de674672720ab7a36
Michael Burton <michaelburton@Michaels-MacBook-Pro.local>
2017-11-13 16:35:25 -0800

    refactored logo typist and particles into separate components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Parallax } from "react-parallax";
 import Nav from "./Nav";
 import Navbar from "./Navbar";
-// import TypistHero from "./TypistHero";
+import TypistHero from "./TypistHero";
 import Particles from "react-particles-js";
 import Typist from "react-typist";
 import Scroll from "react-scroll";
 import params from "./particleParams.js";
+import Logo from "./logo";
 
 let Link = Scroll.Link;
 let Events = Scroll.Events;
@@ -128,208 +129,18 @@ class App extends React.Component {
 
           <div style={{ height: "100vh", display: "block" }}>
             <div style={insideStyles}>
-              {/*
-              <Typist delay={8}>
-
-                <span><h1>Hello, I'm Michael Burton</h1></span>
-                <span><h3>I'm a ...</h3></span>
-                  {this.state.currentMessage} 
-              </Typist>
-          */}
-
               <div>
-                <svg
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 100 100"
-                  enable-background="new 0 0 100 100"
-                  xmlSpace="preserve"
-                  class="bottom"
-                >
-                  <g id="Layer_1" />
-                  <g id="Layer_2">
-                    <path
-                      class="draw"
-                      d="M50,2.5C23.8,2.5,2.5,23.8,2.5,50c0,26.2,21.3,47.5,47.5,47.5S97.5,76.2,97.5,50C97.5,23.8,76.2,2.5,50,2.5z M85.1,70.3
-              c-4.7,8.2-12.2,14.5-21.2,17.8c-4.3,1.6-9,2.4-13.8,2.4s-9.5-0.9-13.8-2.4c-9-3.3-16.5-9.6-21.2-17.8c-3.4-6-5.4-12.9-5.4-20.3
-              c0-2.4,0.2-4.8,0.6-7c1.7-9.6,6.7-18,13.9-24c7-5.9,16.1-9.5,26-9.5s19,3.6,26,9.5c7.1,6,12.2,14.4,13.9,24c0.4,2.3,0.6,4.6,0.6,7
-              C90.5,57.4,88.5,64.3,85.1,70.3z"
-                    />
-                  </g>
-                  <g id="Layer_3">
-                    <path
-                      class="draw"
-                      fill="none"
-                      stroke="#000000"
-                      stroke-miterlimit="10"
-                      d="M50,0C22.4,0,0,22.4,0,50s22.4,50,50,50s50-22.4,50-50S77.6,0,50,0z
-               M85.1,70.3c-4.7,8.2-12.2,14.5-21.2,17.8c-4.3,1.6-9,2.4-13.8,2.4s-9.5-0.9-13.8-2.4c-9-3.3-16.5-9.6-21.2-17.8
-              c-3.4-6-5.4-12.9-5.4-20.3c0-2.4,0.2-4.8,0.6-7c1.7-9.6,6.7-18,13.9-24c7-5.9,16.1-9.5,26-9.5s19,3.6,26,9.5
-              c7.1,6,12.2,14.4,13.9,24c0.4,2.3,0.6,4.6,0.6,7C90.5,57.4,88.5,64.3,85.1,70.3z"
-                    />
-                  </g>
-                  <g id="Layer_4">
-                    <g>
-                      <path
-                        class="draw"
-                        fill="none"
-                        stroke="#FFFFFF"
-                        stroke-miterlimit="10"
-                        d="M94.3,42.2c-1.9-10.6-7.5-20-15.4-26.7C71.1,9,61,5,50,5
-                S28.9,9,21.1,15.5v0c-7.9,6.7-13.5,16-15.4,26.7C5.2,44.7,5,47.3,5,50c0,8.2,2.2,15.9,6,22.5c5.2,9.1,13.6,16.2,23.6,19.8
-                C39.4,94,44.6,95,50,95s10.6-1,15.4-2.7c10-3.6,18.3-10.7,23.6-19.8c3.8-6.6,6-14.3,6-22.5C95,47.3,94.8,44.7,94.3,42.2z
-                 M85.1,70.3c-4.7,8.2-12.2,14.5-21.2,17.8c-4.3,1.6-9,2.4-13.8,2.4s-9.5-0.9-13.8-2.4c-9-3.3-16.5-9.6-21.2-17.8
-                c-3.4-6-5.4-12.9-5.4-20.3c0-2.4,0.2-4.8,0.6-7c1.7-9.6,6.7-18,13.9-24c7-5.9,16.1-9.5,26-9.5s19,3.6,26,9.5
-                c7.1,6,12.2,14.4,13.9,24c0.4,2.3,0.6,4.6,0.6,7C90.5,57.4,88.5,64.3,85.1,70.3z"
-                      />
-                      <path
-                        class="draw"
-                        fill="none"
-                        stroke="#FFFFFF"
-                        stroke-miterlimit="10"
-                        d="M90.5,50c0,7.4-2,14.3-5.4,20.3c-4.7,8.2-12.2,14.5-21.2,17.8
-                c-4.3,1.6-9,2.4-13.8,2.4s-9.5-0.9-13.8-2.4c-9-3.3-16.5-9.6-21.2-17.8c-3.4-6-5.4-12.9-5.4-20.3c0-2.4,0.2-4.8,0.6-7
-                c1.7-9.6,6.7-18,13.9-24c7-5.9,16.1-9.5,26-9.5s19,3.6,26,9.5c7.1,6,12.2,14.4,13.9,24C90.3,45.2,90.5,47.6,90.5,50z"
-                      />
-                      <g>
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="50"
-                          y1="9.5"
-                          x2="50"
-                          y2="5"
-                        />
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="78.9"
-                          y1="15.5"
-                          x2="76"
-                          y2="19"
-                        />
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="94.3"
-                          y1="42.2"
-                          x2="89.9"
-                          y2="43"
-                        />
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="89"
-                          y1="72.5"
-                          x2="85.1"
-                          y2="70.3"
-                        />
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="36.2"
-                          y1="88.1"
-                          x2="34.6"
-                          y2="92.3"
-                        />
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="14.9"
-                          y1="70.3"
-                          x2="11"
-                          y2="72.5"
-                        />
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="10.1"
-                          y1="43"
-                          x2="5.7"
-                          y2="42.2"
-                        />
-                        <line
-                          class="vert"
-                          fill="none"
-                          stroke="#FFFFFF"
-                          stroke-miterlimit="10"
-                          x1="24"
-                          y1="19"
-                          x2="21.1"
-                          y2="15.5"
-                        />
-                      </g>
-                    </g>
-                  </g>
-                  <g id="Layer_5">
-                    <circle cx="50" cy="50" r="40" />
-                  </g>
-                  <g id="Layer_6">
-                    <g>
-                      <rect
-                        x="8.8"
-                        y="24.8"
-                        fill="none"
-                        width="82.3"
-                        height="42.5"
-                      />
-                      <path
-                        fill="#FFFFFF"
-                        d="M16.2,44.5c0-2.7-0.1-5-0.2-7.2h4.2l0.2,4.3h0.2c1.5-2.5,4-4.9,8.4-4.9c3.6,0,6.4,2.2,7.5,5.3h0.1
-                c0.8-1.5,1.9-2.6,3-3.5c1.6-1.2,3.4-1.9,5.9-1.9c3.5,0,8.7,2.3,8.7,11.5v15.7h-4.7V48.8c0-5.1-1.9-8.2-5.8-8.2
-                c-2.7,0-4.9,2-5.7,4.4c-0.2,0.7-0.4,1.5-0.4,2.4v16.4h-4.7V47.9c0-4.2-1.9-7.3-5.6-7.3c-3,0-5.2,2.4-6,4.8
-                c-0.3,0.7-0.4,1.5-0.4,2.4v16.1h-4.7V44.5z"
-                      />
-                      <path
-                        fill="#FFFFFF"
-                        d="M62.2,27.3c2.1-0.4,5.4-0.8,8.7-0.8c4.8,0,7.9,0.8,10.2,2.7c1.9,1.4,3.1,3.6,3.1,6.5
-                c0,3.6-2.4,6.7-6.3,8.1V44c3.5,0.9,7.6,3.8,7.6,9.3c0,3.2-1.3,5.6-3.1,7.4c-2.6,2.4-6.8,3.5-12.8,3.5c-3.3,0-5.8-0.2-7.4-0.4V27.3
-                z M67,42.5h4.3c5.1,0,8-2.6,8-6.2c0-4.3-3.3-6-8.1-6c-2.2,0-3.5,0.2-4.2,0.3V42.5z M67,60.3c0.9,0.2,2.3,0.2,4,0.2
-                c4.9,0,9.5-1.8,9.5-7.2c0-5.1-4.3-7.1-9.6-7.1h-4V60.3z"
-                      />
-                    </g>
-                  </g>
-                </svg>
+                <Logo />
               </div>
-
-              <Typist>
-                <span className="my-custom-class">
-                  <h1>Formally, people call me Michael Burton</h1>
-                </span>
-                <br />
-                <div className="container">
-                  <h3>
-                    <p>I like technology, guitars, and motorcycles</p>
-                    <p>I'm here to create things for people to use and love</p>
-                  </h3>
-                </div>
-              </Typist>
+              <TypistHero />
             </div>
-              <div>
+            <div>
               <svg class="arrows">
-                            <path class="a1" d="M0 0 L30 32 L60 0"></path>
-                            <path class="a2" d="M0 20 L30 52 L60 20"></path>
-                            <path class="a3" d="M0 40 L30 72 L60 40"></path>
-                          </svg>
-              </div>
-
+                <path class="a1" d="M0 0 L30 32 L60 0" />
+                <path class="a2" d="M0 20 L30 52 L60 20" />
+                <path class="a3" d="M0 40 L30 72 L60 40" />
+              </svg>
+            </div>
           </div>
         </div>
         <Element name="weddings">
--- src/TypistHero.js
+++ src/TypistHero.js
@@ -2,47 +2,20 @@ import React from "react";
 import Typist from "react-typist";
 
 export default class TypistHero extends React.PureComponent {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     messages: [
-  //       "coder",
-  //       "photographer",
-  //       "musician",
-  //       "human",
-  //       "designer",
-  //       "programmer"
-  //     ],
-  //     currentMessage: "coder",
-  //     counter: 1
-  //   };
-  //   this.init = this.init.bind(this);
-  //   this.updateMessage = this.updateMessage.bind(this);
-  // }
-  // componentDidMount() {
-  //   this.updateMessage();
-  // }
-  // componentWillUpdate(){
 
-  // }
-  // updateMessage() {
-  //   console.log("updating message typist hero", this.state.currentMessage);
-  //   setInterval(this.init, 4000);
-  // }
-  // init() {
-  //   console.log("currentMessage TypistHero", this.state.currentMessage);
-  //   this.setState({
-  //     currentMessage: this.state.messages[this.state.counter],
-  //     counter:
-  //       this.state.counter < this.state.messages.length - 1
-  //         ? this.state.counter + 1
-  //         : 0
-  //   });
-  // }
   render() {
     return (
       <Typist>
-        <h2>hasfd{this.props.currentMessage}</h2>
+        <span className="my-custom-class">
+          <h1>Formally, people call me Michael Burton</h1>
+        </span>
+        <br />
+        <div className="container">
+          <h3>
+            <p>I like technology, guitars, and motorcycles</p>
+            <p>I'm here to create things for people to use and love</p>
+          </h3>
+        </div>
       </Typist>
     );
   }
